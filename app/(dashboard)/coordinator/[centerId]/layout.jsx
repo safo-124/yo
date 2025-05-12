dcdf2e7cca2a208f700152c50f0cd0f06efa2f65
Safo Emmanuel <nanasafo122@gmail.com>
2025-05-12 06:55:52 +0000

    38

--- app/(dashboard)/coordinator/[centerId]/layout.jsx
+++ app/(dashboard)/coordinator/[centerId]/layout.jsx
@@ -105,11 +105,7 @@ export default async function CoordinatorLayout({ children, params }) {
           {children}
         </main>
         
-        <footer className="bg-white dark:bg-gray-800 border-t border-gray-200 dark:border-gray-700">
-          <div className="mx-auto py-4 px-4 sm:px-6 lg:px-8 text-center text-gray-500 dark:text-gray-400 text-sm max-w-[80vw]">
-            © {new Date().getFullYear()} Your Application Name. All rights reserved.
-          </div>
-        </footer>
+    
       </div>
     </div>
   );
