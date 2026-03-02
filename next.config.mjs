ea6c8f34812880f93666e846ec671e980261a777
safo-124 <nanasafo122@gmail.com>
2026-03-02 19:29:42 +0200

    100

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  turbopack: {},
   webpack: (config) => {
-    // This line solves the "Can't resolve 'fs'" issue by ignoring the fs module
-    // in browser bundles, since we only use it in server components now
     config.resolve.fallback = { fs: false, path: false };
     return config;
   },
